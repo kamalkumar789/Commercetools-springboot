4d0e69bee362b642d74e1f053dcc896f48cacb43
kamal <kamalkumargit@gmail.com>
2023-03-13 01:43:45 +0500

    changes

--- src/main/java/com/example/commercetools/CommercetoolsApplication.java
+++ src/main/java/com/example/commercetools/CommercetoolsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class CommercetoolsApplication {
 
 	public static void main(String[] args) {
@@ -16,4 +17,5 @@ public class CommercetoolsApplication {
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
+
 }
